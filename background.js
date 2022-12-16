ef56c9e4af88a12323ac0c2720264cf82667c362
Younes Alturkey <hi@younes.ninja>
2022-12-16 23:00:30 +0300

    api-showcase

--- background.js
+++ background.js
@@ -1,34 +1,32 @@
-chrome.alarms.create("pomodoroTimer", {
-    periodInMinutes: 1 / 60,
-})
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === "pomodoroTimer") {
-        chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
-            if (res.isRunning) {
-                let timer = res.timer + 1
-                let isRunning = true
-                if (timer === 60 * res.timeOption) {
-                    this.registration.showNotification("Pomodoro Timer", {
-                        body: `${res.timeOption} minutes has passed!`,
-                        icon: "icon.png",
-                    })
-                    timer = 0
-                    isRunning = false
-                }
-                chrome.storage.local.set({
-                    timer,
-                    isRunning,
-                })
-            }
-        })
-    }
-})
-
-chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+chrome.runtime.onInstalled.addListener((details) => {
     chrome.storage.local.set({
-        timer: "timer" in res ? res.timer : 0,
-        timeOption: "timeOption" in res ? res.timeOption : 25,
-        isRunning: "isRunning" in res ? res.isRunning : false,
+        shows: [],
+    })
+    chrome.contextMenus.create({
+        title: "Search TV Show",
+        id: "contextMenu1",
+        contexts: ["page", "selection"]
+    })
+    chrome.contextMenus.create({
+        title: "Read This Text",
+        id: "contextMenu2",
+        contexts: ["page", "selection"]
+    })
+    chrome.contextMenus.onClicked.addListener((event) => {
+        if (event.menuItemId === "contextMenu1") {
+            fetch(`http://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    chrome.storage.local.set({
+                        shows: data,
+                    })
+                })
+        } else if (event.menuItemId === "contextMenu2") {
+            chrome.tts.speak(event.selectionText, {
+                lang: "zh-CN",
+                rate: 1,
+            })
+        }
     })
 })
--- popup/popup.js
+++ popup/popup.js
@@ -1,101 +1,20 @@
-let tasks = []
-
-function updateTime() {
-    chrome.storage.local.get(["timer", "timeOption", "isRunning"], (res) => {
-        const time = document.getElementById("time")
-        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0")
-        let seconds = "00"
-        if (res.timer % 60 != 0) {
-            seconds = `${60 - res.timer % 60}`.padStart(2, "0")
-        }
-        time.textContent = `${minutes}:${seconds}`
-        startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer"
-    })
-}
-
-updateTime()
-setInterval(updateTime, 1000)
-
-const startTimerBtn = document.getElementById("start-timer-btn")
-startTimerBtn.addEventListener("click", () => {
-    chrome.storage.local.get(["isRunning"], (res) => {
-        chrome.storage.local.set({
-            isRunning: !res.isRunning,
-        }, () => {
-            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
-        })
-    })
+chrome.storage.local.get(["shows"], (res) => {
+    console.log(res)
+    for (const show of res.shows) {
+        renderShow(show)
+    }
 })
 
-const resetTimerBtn = document.getElementById("reset-timer-btn")
-resetTimerBtn.addEventListener("click", () => {
-    chrome.storage.local.set({
-        timer: 0,
-        isRunning: false,
-    }, () => {
-        startTimerBtn.textContent = "Start Timer"
-    })
-})
+function renderShow(show) {
+    const showDiv = document.createElement("div")
 
-const addTaskBtn = document.getElementById("add-task-btn")
-addTaskBtn.addEventListener("click", () => addTask())
+    const title = document.createElement("h3")
+    title.textContent = show.show.name
 
-chrome.storage.sync.get(["tasks"], (res) => {
-    tasks = res.tasks ? res.tasks : []
-    renderTasks()
-})
+    const image = document.createElement("img")
+    image.src = show.show.image ? show.show.image.medium : null
 
-function saveTasks() {
-    chrome.storage.sync.set({
-        tasks,
-    })
-}
-
-function renderTask(taskNum) {
-    const taskRow = document.createElement("div")
-
-    const text = document.createElement("input")
-    text.type = "text"
-    text.placeholder = "Enter a task..."
-    text.value = tasks[taskNum]
-    text.className = "task-input"
-    text.addEventListener("change", () => {
-        tasks[taskNum] = text.value
-        saveTasks()
-    })
-
-    const deleteBtn = document.createElement("input")
-    deleteBtn.type = "button"
-    deleteBtn.value = "X"
-    deleteBtn.className = "task-delete"
-    deleteBtn.addEventListener("click", () => {
-        deleteTask(taskNum)
-    })
-
-    taskRow.appendChild(text)
-    taskRow.appendChild(deleteBtn)
-
-    const taskContainer = document.getElementById("task-container")
-    taskContainer.appendChild(taskRow)
-}
-
-function addTask() {
-    const taskNum = tasks.length
-    tasks.push("")
-    renderTask(taskNum)
-    saveTasks()
-}
-
-function deleteTask(taskNum) {
-    tasks.splice(taskNum, 1)
-    renderTasks()
-    saveTasks()
-}
-
-function renderTasks() {
-    const taskContainer = document.getElementById("task-container")
-    taskContainer.textContent = ""
-    tasks.forEach((taskText, taskNum) => {
-        renderTask(taskNum)
-    })
+    showDiv.appendChild(title)
+    showDiv.appendChild(image)
+    document.body.appendChild(showDiv)
 }
